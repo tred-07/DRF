70fdd4392b93c91976f8dc8cb5f442ba12fcc68d
Arannamoy-Mondal <arannamoymondal13@gmail.com>
2024-12-20 02:49:30 +0600

    upd

--- p1_watchlist_app/watchmate/serializers.py
+++ p1_watchlist_app/watchmate/serializers.py
@@ -1,9 +1,13 @@
 from rest_framework import serializers
 from .models import Movie
 class MovieSerializer(serializers.Serializer): # General Serializer
-    class Meta:
-        model=Movie # Select Model
-        fields='__all__' # List of fields
+    id=serializers.IntegerField(read_only=True)
+    name=serializers.CharField()
+    description=serializers.CharField()
+    active=serializers.BooleanField()
+    # class Meta:
+        # model=Movie # Select Model
+        # fields='__all__' # List of fields
 
 
 class MovieModelSerializer(serializers.ModelSerializer): # Model serializer
--- p1_watchlist_app/watchmate/views.py
+++ p1_watchlist_app/watchmate/views.py
@@ -3,14 +3,15 @@ from .models import Movie
 from django.http import HttpResponse,JsonResponse
 from rest_framework import viewsets
 from .serializers import MovieModelSerializer,MovieSerializer
+from rest_framework import decorators,response
 # Create your views here.
 
 
-def movie_list(request):
-    movies = Movie.objects.all() # it creates all objects from model
-    print (movies.values()) # prints all objects
-    data={'movies':list(movies.values())} # converts all objects to json
-    return JsonResponse(data) # returns json response
+# def movie_list(request):
+#     movies = Movie.objects.all() # it creates all objects from model
+#     print (movies.values()) # prints all objects
+#     data={'movies':list(movies.values())} # converts all objects to json
+#     return JsonResponse(data) # returns json response
 
 def movie_detail(request, pk):
     try:
@@ -27,3 +28,9 @@ def movie_detail(request, pk):
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieModelSerializer # using serializer from serializers.py
+
+@decorators.api_view(['GET']) # from rest_framework import decorators
+def movie_list(request):
+    movie=Movie.objects.all()
+    serializer=MovieSerializer(movie,many=True) # many=true means it check all object of serializer individually.
+    return response.Response(serializer.data)
