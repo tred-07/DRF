e10b523266ff7ee3dcec9270782a33235ff1391d
tred-07 <aranna.rtx.bd.02@gmail.com>
2024-12-30 19:54:07 +0600

    add default log in

--- p1_watchlist_app/p1_watchlist_app/urls.py
+++ p1_watchlist_app/p1_watchlist_app/urls.py
@@ -4,4 +4,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('app/', include('watchmate.urls')),  # Add this line
+    path('api-auth/', include('rest_framework.urls')),  # Add this line
 ]
