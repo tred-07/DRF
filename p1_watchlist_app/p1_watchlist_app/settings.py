83ae61fcee9bd3b875f68f768a722288e26169ed
tred-07 <aranna.rtx.bd.02@gmail.com>
2025-01-01 01:05:43 +0600

    add JWT

--- p1_watchlist_app/p1_watchlist_app/settings.py
+++ p1_watchlist_app/p1_watchlist_app/settings.py
@@ -130,5 +130,6 @@ REST_FRAMEWORK = {
      'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ]
 }
--- p1_watchlist_app/p1_watchlist_app/urls.py
+++ p1_watchlist_app/p1_watchlist_app/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path,include
+from rest_framework_simplejwt.views import (TokenObtainPairView,TokenRefreshView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('app/', include('watchmate.urls')),  # Add this line
     path('api-auth/', include('rest_framework.urls')),  # Add this line,
     path('user/', include('user_app.urls')),  # Add this line
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
