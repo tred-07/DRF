01d303ec24bd48b9418280d908116dc9d28a155c
tred-07 <aranna.rtx.bd.02@gmail.com>
2024-12-31 20:50:50 +0600

    create user_app

--- p1_watchlist_app/p1_watchlist_app/settings.py
+++ p1_watchlist_app/p1_watchlist_app/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'watchmate'
+    'watchmate',
+    'user_app',
 ]
 
 MIDDLEWARE = [
--- p1_watchlist_app/p1_watchlist_app/urls.py
+++ p1_watchlist_app/p1_watchlist_app/urls.py
@@ -4,5 +4,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('app/', include('watchmate.urls')),  # Add this line
-    path('api-auth/', include('rest_framework.urls')),  # Add this line
+    path('api-auth/', include('rest_framework.urls')),  # Add this line,
+    path('user/', include('user_app.urls')),  # Add this line
 ]
