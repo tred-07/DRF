ff6ac04d76c1210097c5c815681498c722558ed8
tred-07 <aranna.rtx.bd.02@gmail.com>
2025-01-02 03:46:20 +0600

    update

--- p1_watchlist_app/watchmate/pagination.py
+++ p1_watchlist_app/watchmate/pagination.py
@@ -8,4 +8,4 @@ class WatchListPagination(pagination.PageNumberPagination):
 class ReviewListPagination(pagination.PageNumberPagination):
     page_size=2 # number of items per page
     page_size_query_param='page_size' # parameter for pagination
-    max_page_size=10    # maximum page size
+    max_page_size=10    # maximum page sizez
